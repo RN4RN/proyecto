669e439b884a86fa67f5be25d31b687712e9c5e5
RN4RN <grijalvauve@gmail.com>
2025-06-02 11:27:34 -0500

    Delete public/contraseña/mapaprincipal/conexion.php

--- public/contraseña/mapaprincipal/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$clave = ""; 
-$base_datos = "gestion_equipos_telecom";
-
-$conexion = new mysqli($servidor, $usuario, $clave, $base_datos);
-
-?>
