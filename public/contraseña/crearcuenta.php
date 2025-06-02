70ef3d28577534bb71334f8136850f9d3ffac9ca
RN4RN <grijalvauve@gmail.com>
2025-06-02 12:57:12 -0500

    Update crearcuenta.php

--- public/contraseña/crearcuenta.php
+++ public/contraseña/crearcuenta.php
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             timer: 3000,
                             showConfirmButton: false
                         }).then(() => {
-                            window.location.href = '/contrase%C3%B1a/index.php';
+                            window.location.href = '/index.php';
                         });
                     });
                 </script>";
